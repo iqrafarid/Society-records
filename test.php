53aa0ef5b1da9822ad3894648cb13a8d5e25e726
iqrafarid <iqra.farid@somaiya.edu>
2025-09-20 22:22:04 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,12 +1,13 @@
 <?php
                 $hostName = "localhost"; 
                 $dbUser = "root";
-                $dbPass = "F@123iqr";    //mysql server username
-                $dbName = "Society_records";       //database name
+                $dbPass = "F@123iqr1211";    //mysql server username
+                $dbName = "societyrecords";       //database name
                 $conn = mysqli_connect($hostName, $dbUser, $dbPass, $dbName);
                 if (!$conn) {
                     die("Connection is not successful!");
                 }
                 $sql = "SELECT * FROM Owners";
                 $result = mysqli_query($conn, $sql);
+
 ?>
